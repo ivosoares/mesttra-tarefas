69ae8ba04c87b1e38bd4889672f612f80c646da9
Ivo Soares <ivonaldojrsoares@gmail.com>
2023-11-23 22:09:48 -0300

    aula 04  - Adiciona busca de pokemon (o misterio da virgula)

--- pokedex/scripts.js
+++ pokedex/scripts.js
@@ -1,17 +1,21 @@
 const listaTarefasHtml = document.getElementById('pokemons');
 
 
-const getPokemons = () => {
-  const Api = fetch('https://pokeapi.co/api/v2/pokemon?limit=500');
-  Api.then((response) => {
-    console.log(response);
-    return response.json();
-  }).then((data) => {
-    render(data.results);
-  })
+const getPokemons = async (name = '') => {
+  if (name !== '') {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    const data = await response.json();
+    renderOne(data);
+  }else {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=500');
+    const data = await response.json();
+    renderAll(data.results);
+  }
+
 }
 
-const render = (pokemons) => {
+const renderAll = (pokemons) => {
+  listaTarefasHtml.innerHTML = '';
   pokemons.map((pokemon, index) => {
     listaTarefasHtml.insertAdjacentHTML('beforeend', 
     `
@@ -23,5 +27,51 @@ const render = (pokemons) => {
   })
 }
 
+const renderOne = (pokemon) => {
+  console.log(pokemon);
+  listaTarefasHtml.innerHTML = '';
+  listaTarefasHtml.insertAdjacentHTML('beforeend', 
+  `
+    <li class="pokemon-card">
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/${pokemon.id}.gif"/>
+      <h3>${pokemon.name}</h3>
+      <h4>Peso: ${pokemon.weight}</h4>
+      <p>Tipo:</p>
+      ${pokemon.types.map(tipoPokemon => { 
+        if(tipoPokemon.type.name == 'poison') {
+          return `
+          <h2 id="poison">
+              ${tipoPokemon.type.name}
+          </h2>
+          `
+        }else {
+          return `
+          <h2>
+              ${tipoPokemon.type.name}
+          </h2>
+          `
+        }
+        })}
+    </li>
+  `)
+}
+
+const searchPokemon = (event) => {
+  // previno o comportamento padrao do evento submit (atualizar a pagina);
+  event.preventDefault();
+  const text = document.getElementById('buscarInput').value;
+  getPokemons(text);
+}
+
 getPokemons();
 
+
+
+
+// const minhaFuncaoAssincrona = async () => {
+//   const response = await fetch('http://localhost:3000/tarefas')
+//   console.log(response);
+//   const tarefas = await response.json();
+//   console.log(tarefas);
+// }
+// minhaFuncaoAssincrona();
